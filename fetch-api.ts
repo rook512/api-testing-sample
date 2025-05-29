a63fffb3eaf41dbf6469a6f3640272032fa6b712
rook512 <31941582+rook512@users.noreply.github.com>
2025-05-28 19:43:53 -0500

    updates fetch call to use host

--- fetch-api.ts
+++ fetch-api.ts
@@ -2,6 +2,6 @@ import {toFetchApi} from '@darkpatternsdigital/openapi-codegen-typescript-fetch'
 import operations from './api-generated/operations'
 
 export const api = toFetchApi(operations, async (url, req) => {
-    const result = await fetch(url, req);
+    const result = await fetch("https://www.dnd5eapi.co" + url, req);
     return result
 })
